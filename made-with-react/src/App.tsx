3e354ddd8d53d77eafd4c7af66e1d47b496953cf
riley0122 <77288737+riley0122@users.noreply.github.com>
2024-03-21 18:09:51 +0100

    Added dog images

--- made-with-react/src/App.tsx
+++ made-with-react/src/App.tsx
@@ -1,11 +1,59 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
+import Card from './Card';
+
+interface pair {
+  firstCard: number;
+  secondCard: number;
+  dog: string;
+}
 
 function App() {
+  const cardCount = 16;
+  const [pairs, setPairs] = React.useState([] as pair[]);
+  let unassigned = [...Array(cardCount).keys()];
+  const [flippedCards, setFlippedCards] = React.useState([...Array(cardCount).keys()].map(card => {
+    return false;
+  }));
+
+  useEffect(() => {
+    for (let i = 0; i < [...Array(cardCount/2)].length; i++) {
+      fetch('https://random.dog/woof.json').then(res => {return res.json()}).then(data => {
+        let random = Math.floor(Math.random() * unassigned.length);
+        const firstCard = unassigned.splice(random, 1)[0];
+        random = Math.floor(Math.random() * unassigned.length);
+        const secondCard = unassigned.splice(random, 1)[0];
+        let p = pairs;
+        p.push({
+          firstCard: firstCard,
+          secondCard: secondCard,
+          dog: data["url"],
+        } as pair);
+        setPairs(p);
+      });
+    }
+  }, []);
+
+  function flipCard(index: number) {
+    setFlippedCards(flippedCards.map((card, i) => {
+      if (i === index) {
+        return !card;
+      } else {
+        return card;
+      }
+    }));
+  }
+
+  function getDog(index: number) {
+    return pairs.filter((value) => {return value.firstCard === index || value.secondCard === index})[0]?.dog || "https://gifdb.com/images/high/shiba-inu-hunting-dog-loading-fake-smile-jrnqgz1xt5xgmy3f.gif";
+  }
+
   return (
     <div className="App">
-      {[...Array(16)].map((_, i) => (
-        <div key={i} className={"shadow-[0px_0px_5px_2px] shadow-slate-700 rounded-lg ease-linear duration-150 hover:shadow-[0px_0px_20px_10px] hover:shadow-slate-400 div" + (i + 1).toString()}></div>
+      {[...Array(cardCount)].map((_, i) => (
+        <div key={i} onClick={() => !flippedCards[i] ? flipCard(i) : {}} className='w-full h-full' >
+          <Card index={i} flip={flippedCards[i]} dog={getDog(i)}/>
+        </div>
       ))}
     </div>
   );
---
+++ made-with-react/src/Card.tsx
@@ -0,0 +1,7 @@
+export default function Card(props: { index: number; flip: boolean; dog: string; }){
+    return (
+        <div key={props.index} className={(props.flip ? "" : "cursor-pointer ") + "w-full h-full shadow-[0px_0px_5px_2px] shadow-slate-700 rounded-lg ease-linear duration-150 hover:shadow-[0px_0px_20px_10px] hover:shadow-slate-400 justify-center items-center div grow-0" + (props.index + 1).toString()}>
+          {props.flip ? (props.dog.endsWith(".mp4") ? <video autoPlay loop muted className="object-scale-down w-full h-full object-center m-0" ><source src={props.dog} type="video/mp4" />Your browser does not support the video tag.</video> : <img src={props.dog} className="object-scale-down w-full h-full object-center m-0" alt="doggo" />) : (props.index + 1).toString()}
+        </div>
+    )
+}
