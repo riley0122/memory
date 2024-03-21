39c26eed23c401575dfdb805224afd75f5b875f3
riley0122 <77288737+riley0122@users.noreply.github.com>
2024-03-21 16:39:42 +0100

    some styling

--- made-with-react/src/App.tsx
+++ made-with-react/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       {[...Array(16)].map((_, i) => (
-        <div key={i} className={"div" + (i + 1).toString()}></div>
+        <div key={i} className={"shadow-[0px_0px_5px_2px] shadow-slate-700 rounded-lg ease-linear duration-150 hover:shadow-[0px_0px_20px_10px] hover:shadow-slate-400 div" + (i + 1).toString()}></div>
       ))}
     </div>
   );
